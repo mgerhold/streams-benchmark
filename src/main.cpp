89316e3e05443056ab8a2dcc5ac81af653f58cfc
Michael Gerhold <michael.gerhold@web.de>
2024-10-27 08:31:56 +0100

    feat: add additional benchmarks

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <benchmark/benchmark.h>
 #include <array>
-#include <vector>
-#include <string>
+#include <charconv>
+#include <cstdio>
+#include <format>
+#include <limits>
 #include <sstream>
+#include <string>
+#include <vector>
 
 template<std::size_t count>
 [[nodiscard]] static consteval auto create_letters() {
@@ -13,6 +17,16 @@ template<std::size_t count>
         if (letter > 'z') {
             letter = 'a';
         }
+    }
+    return result;
+}
+
+template<std::size_t count>
+[[nodiscard]] static consteval auto create_numbers() {
+    auto result = std::array<double, count>{};
+    for (auto x = 0.1; auto& current : result) {
+        current = x;
+        x += 0.1;
     }
     return result;
 }
@@ -108,6 +122,67 @@ static void BM_StringStream_Big(benchmark::State& state) {
     }
 }
 
+static constexpr auto num_doubles = std::size_t{ 1024 };
+
+static void BM_Format_Doubles_Snprintf(benchmark::State& state) {
+    static constexpr auto numbers = create_numbers<num_doubles>();
+    for (auto _ : state) {
+        auto array = std::array<char, std::numeric_limits<double>::max_digits10>{};
+        for (auto const number : numbers) {
+            std::snprintf(array.data(), array.size(), "%lf", number);
+        }
+        benchmark::DoNotOptimize(array);
+    }
+}
+
+static void BM_Format_Doubles_To_Chars(benchmark::State& state) {
+    static constexpr auto numbers = create_numbers<num_doubles>();
+    for (auto _ : state) {
+        auto array = std::array<char, std::numeric_limits<double>::max_digits10>{};
+        for (auto const number : numbers) {
+            std::to_chars(array.data(), array.data() + array.size(), number);
+        }
+        benchmark::DoNotOptimize(array);
+    }
+}
+
+static void BM_Format_Doubles_Format_To_Vector_Back_Inserter(benchmark::State& state) {
+    static constexpr auto numbers = create_numbers<num_doubles>();
+    static const auto locale = std::locale{ "de_DE.UTF-8" };
+    for (auto _ : state) {
+        auto vector = std::vector<char>{};
+        for (auto const number : numbers) {
+            std::format_to(std::back_inserter(vector), locale, "{}", number);
+        }
+        benchmark::DoNotOptimize(vector);
+    }
+}
+
+static void BM_Format_Doubles_Format_String(benchmark::State& state) {
+    static constexpr auto numbers = create_numbers<num_doubles>();
+    static const auto locale = std::locale{ "de_DE.UTF-8" };
+    for (auto _ : state) {
+        auto string = std::string{};
+        for (auto const number : numbers) {
+            string += std::format(locale, "{}", number);
+        }
+        benchmark::DoNotOptimize(string);
+    }
+}
+
+static void BM_Format_Doubles_String_Stream(benchmark::State& state) {
+    static constexpr auto numbers = create_numbers<num_doubles>();
+    static const auto locale = std::locale{ "de_DE.UTF-8" };
+    for (auto _ : state) {
+        auto stream = std::stringstream{};
+        stream.imbue(locale);
+        for (auto const number : numbers) {
+            stream << number;
+        }
+        benchmark::DoNotOptimize(stream);
+    }
+}
+
 BENCHMARK(BM_Array);
 BENCHMARK(BM_Vector);
 BENCHMARK(BM_String);
@@ -116,5 +191,11 @@ BENCHMARK(BM_Array_Big);
 BENCHMARK(BM_Vector_Big);
 BENCHMARK(BM_String_Big);
 BENCHMARK(BM_StringStream_Big);
+
+BENCHMARK(BM_Format_Doubles_Snprintf);
+BENCHMARK(BM_Format_Doubles_To_Chars);
+BENCHMARK(BM_Format_Doubles_Format_To_Vector_Back_Inserter);
+BENCHMARK(BM_Format_Doubles_Format_String);
+BENCHMARK(BM_Format_Doubles_String_Stream);
 
 BENCHMARK_MAIN();
